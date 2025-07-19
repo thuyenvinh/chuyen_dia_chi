a1b5a0824ece3ff44decc8fe93ec08bbf18ab42b
Thuyen Nguyen <thuyenvinh@gmail.com>
2025-07-19 04:31:56 +0000

    A

--- test_chuyen.sql
+++ test_chuyen.sql
@@ -2,13 +2,17 @@ create table TEMP_CONVERT_ADD
 (
   id      NUMBER(12) not null,
   old_add VARCHAR2(1000),
-  new_add VARCHAR2(1000)
+  new_add VARCHAR2(1000),
+  MESSAGE VARCHAR2(100)
 );
 
 insert into TEMP_CONVERT_ADD xxxx;
 
+
 Begin
-  For REC In (Select ID, OLD_ADD From TEMP_CONVERT_ADD)
+  For REC In (Select ID, OLD_ADD
+              From   TEMP_CONVERT_ADD A
+              Where  A.NEW_ADD Is Null)
   Loop
     Declare
       V_RESULT ADDRESS_CONVERTER.T_RESULT;
