3121506873301f0f4fca7f17d43e47c54a00aabe
Thuyen Nguyen <thuyenvinh@gmail.com>
2025-07-19 04:30:03 +0000

    thêm test

---
+++ test_chuyen.sql
@@ -0,0 +1,23 @@
+create table TEMP_CONVERT_ADD
+(
+  id      NUMBER(12) not null,
+  old_add VARCHAR2(1000),
+  new_add VARCHAR2(1000)
+);
+
+insert into TEMP_CONVERT_ADD xxxx;
+
+Begin
+  For REC In (Select ID, OLD_ADD From TEMP_CONVERT_ADD)
+  Loop
+    Declare
+      V_RESULT ADDRESS_CONVERTER.T_RESULT;
+    Begin
+      V_RESULT := ADDRESS_CONVERTER.CONVERT_ADDRESS(REC.OLD_ADD);
+      Update TEMP_CONVERT_ADD
+      Set    NEW_ADD = V_RESULT.CONVERTED_ADDRESS,
+             MESSAGE = V_RESULT.MESSAGE
+      Where  ID = REC.ID;
+    End;
+  End Loop;
+End;
